ed2ccb3b6f6e1d0c576986ff7059e8ea4247b9a3
popwandee <popwandee@gmail.com>
2019-07-19 17:23:42 +0700

    Update callback.php

--- callback.php
+++ callback.php
@@ -157,6 +157,7 @@ foreach ($events as $event) {
 		                    $replyData = $multiMessage;
 				}// end no answer, just question only
 				$numImg=mt_rand(0,2);
+				$numImg=2;
 				$json = file_get_contents('https://api.mlab.com/api/1/databases/crma51/collections/img?apiKey='.MLAB_API_KEY.'&q={"no":$numImg}');
                                 $data = json_decode($json);
                                 $isData=sizeof($data);
