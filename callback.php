0064c0cf5ceb46d44510040f7cc6b1b009b9cb12
popwandee <popwandee@gmail.com>
2019-07-20 15:34:04 +0700

    Update callback.php

--- callback.php
+++ callback.php
@@ -143,7 +143,7 @@ foreach ($events as $event) {
                                 $isData=sizeof($data);
                                 if($isData >0){
                                    foreach($data as $rec){
-                                           $textReplyMessage=$explodeText[1].".......\n".$rec->answer."\n";
+                                           $textReplyMessage=$kmText[1].".......\n".$rec->answer."\n";
                                            }//end for each
 					if(isset($rec->Image)){
 		 	                  $picFullSize="https://thaitimes.online/wp-content/uploads/".$rec->Image;
