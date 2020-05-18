33ef01b207dc05cc23cfe43531f6ded8b6dd3733
Onion <popwandee@gmail.com>
2020-05-18 17:58:24 +0700

    ok

--- image.php
+++ image.php
@@ -221,6 +221,25 @@ else{
 }
 
          ?>
+
+         <div class="col-sm-4">
+           <div class="panel panel-warning">
+             <div class="panel-heading">
+               <h3 class="panel-title">ประชาสัมพันธ์</h3>
+             </div>
+             <div class="panel-body">
+               <img src='https://firebasestorage.googleapis.com/v0/b/lisa-77436.appspot.com/o/image%2F1.jpg?alt=media&token=e9d388df-0b6d-4e10-a49c-6472f9aff38a' width='100'>
+             </div>
+           </div>
+           <div class="panel panel-danger">
+             <div class="panel-heading">
+               <h3 class="panel-title">ข้อมูลสมาชิก</h3>
+             </div>
+             <div class="panel-body">
+               <img src='https://firebasestorage.googleapis.com/v0/b/lisa-77436.appspot.com/o/image%2F11.jpg?alt=media&token=dc346d7b-8d66-4861-a2b8-f419f76b471c' width='100'>
+             </div>
+           </div>
+         </div><!-- /.col-sm-4 -->
     </div> <!-- end .container -->
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
