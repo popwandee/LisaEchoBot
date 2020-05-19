be4bb78757f8149793562255e751b4d9dcc34cf0
Onion <popwandee@gmail.com>
2020-05-19 23:37:13 +0700

    debug sql

--- usermanager.php
+++ usermanager.php
@@ -81,7 +81,7 @@ require_once "config.php";
        $json = file_get_contents('https://api.mlab.com/api/1/databases/crma51/collections/manager?apiKey='.MLAB_API_KEY.'&q={"name":{"$regex":"'.$name.'"}}');
             break;
        case "search_username" :
-       $json = file_get_contents('https://api.mlab.com/api/1/databases/crma51/collections/manager?apiKey='.MLAB_API_KEY.'&q={"username":'.$username.'}');
+       $json = file_get_contents('https://api.mlab.com/api/1/databases/crma51/collections/manager?apiKey='.MLAB_API_KEY.'&q={"username":"'.$username.'"}');
             break;
        default :
             foo("no");
