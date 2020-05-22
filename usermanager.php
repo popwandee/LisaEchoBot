9c704607ebedb61eaaca6fd345d3eab5d2b6e81c
Onion <popwandee@gmail.com>
2020-05-22 13:24:09 +0700

    tempolary not login

--- usermanager.php
+++ usermanager.php
@@ -1,7 +1,7 @@
 <?php
 // Initialize the session
 session_start();
-
+/*
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
@@ -11,6 +11,7 @@ if(!isset($_SESSION["type"]) || $_SESSION["type"] !== "admin"){
     header("location: index.php?message=You are not admin.");
     exit;
 }
+*/
 // Include config file
 require_once "config.php";
 
