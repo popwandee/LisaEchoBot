a2e337c9c872b438174f686dd3aca13e8e5a32c1
Onion <popwandee@gmail.com>
2020-05-25 17:06:47 +0700

    request for committee

---
+++ request.php
@@ -0,0 +1,252 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+// Include config file
+require_once "config.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="AFAPS40-CRMA51 Website">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>AFAPS40-CRMA51</title>
+    <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+      <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+      <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+      <script src="vendor/bootstrap/ie-emulation-modes-warning.js"></script>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+    <?php include 'navigation.html';?>
+
+    <div class="container theme-showcase" role="main">
+    <div class="jumbotron">
+          <?php show_all_request();?>
+    <span class="label label-primary">แจ้งกรรมการรุ่นเพื่อทราบและพิจารณา</span>
+    </div>
+    <div class="jumbotron">
+      <?php
+        $action= isset($_GET['action']) ? $_GET['action'] : "";
+        $_id = isset($_GET['_id']) ? $_GET['_id'] : "";
+        if($action == 'review') && !empty($_id){
+          review_request($_id);
+          }?>
+ <?php
+ if(!isset($_POST['formSubmit'])){ // มาจากหน้าอื่นๆ ไม่ได้คลิกยืนยันที่ฟอร์มแก้ไขข้อมูล
+   // ดึงข้อมูลจากฐานข้อมูล
+   $id=$_GET['id'];//echo "GET ID: ".$id;
+   $url="https://api.mlab.com/api/1/databases/crma51/collections/request/".$id."?apiKey=".MLAB_API_KEY;
+   $json = file_get_contents($url);//echo "\nurl is =>".$url;
+   $data = json_decode($json);//echo "\n data is =>";print_r($data);
+   $isData=sizeof($data);
+   $i=0;
+ if($isData >0){
+      // มีข้อมูลผู้ใช้อยู่
+      $name=$data->name;
+      $title=$data->title;
+      $detail=$data->detail;
+      $type=$data->type;
+      $urgent=$data->urgent;
+      $status=$data->status;
+ }// end isData>0
+ else{
+   echo "ยังไม่มีเรื่องแจ้งจากสมาชิก";
+ }
+?>
+	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+    <table class='table table-hover table-responsive table-bordered'>
+        <tr><td>แจ้งเรื่องต่าง ๆ ให้คณะกรรมการรุ่นทราบ</td></tr>
+        <tr><td>
+        <select name="urgent">
+        <option value="เร่งด่วน">เร่งด่วน</option>
+        <option value="ไม่ด่วน">ไม่ด่วน</option>
+        </select>
+          <select name="type">
+            <option value="เพื่อทราบ">เพื่อทราบ</option>
+            <option value="เพื่อพิจารณาดำเนินการ">เพื่อพิจารณาดำเนินการ</option>
+            <option value="เพื่ออนุมัติ">เพื่ออนุมัติ</option>
+          </select></td></tr>
+          <tr>
+              <td>หัวเรื่อง<input type='text' name='title' class='form-control' /></td>
+              </tr>
+            <td>ระบุรายละเอียดข้อมูลที่ต้องการแจ้งกรรมการรุ่น</td>
+            <td><textarea name="comment" rows="10" cols="30"class='form-control' /></textarea></td>
+        </tr>
+    <tr><td>
+        ผู้แจ้ง<input type='hidden' name='name' value="<?php $user_info=isset($_SESSION["user_info"]) ? $_SESSION['user_info'] : ""; echo $user_info;?>" /><?php echo $user_info;?>
+      </td>
+  </tr>
+        <tr>
+            <td></td>
+            <td>  <input type="hidden"name="id" value="<?php echo $id;?>">
+                  <input type="hidden"name="formSubmit" value="true">
+                <input type='submit' value='Save' class='btn btn-primary' />
+
+            </td>
+        </tr>
+    </table>
+</form>
+<?php  } // end if empty $formSubmit
+else{ // set formSubmit from form
+  // นำข้อมูลเข้าเก็บในฐานข้อมูล
+  if(isset($_POST['name'])){$name=$_POST['name'];}else{$name=''; }
+  if(isset($_POST['title'])){$title=$_POST['title'];}else{$title=''; }
+  if(isset($_POST['detail'])){$detail=$_POST['detail'];}else{$detail=''; }
+  if(isset($_POST['type'])){$type=$_POST['type'];}else{$type=''; }
+  if(isset($_POST['urgent'])){$urgent=$_POST['urgent'];}else{$urgent=''; }
+  if(isset($_POST['status'])){$status=$_POST['status'];}else{$status=''; }
+  $newData = json_encode(array(
+    'name'=>$name,
+    'title' => $title,
+    'detail' => $detail,
+    'type' => $type ,
+  	'urgent' => $urgent,
+    'status'=>'แจ้งใหม่') );
+  $opts = array('http' => array( 'method' => "POST",
+                                 'header' => "Content-type: application/json",
+                                 'content' => $newData
+                                             )
+                                          );
+  $url = 'https://api.mlab.com/api/1/databases/crma51/collections/request?apiKey='.MLAB_API_KEY.'';
+          $context = stream_context_create($opts);
+          $returnValue = file_get_contents($url,false,$context);
+
+          if($returnValue){
+  		   $message= "<div align='center' class='alert alert-success'>รับแจ้งข้อมูล ".$title." เรียบร้อย</div>";
+  		   echo $message;
+
+  	        }else{
+  		   $message= "<div align='center' class='alert alert-danger'>ไม่สามารถบันทึกรับแจ้งการข้อมูลได้</div>";
+  		echo $message;
+                   }
+  			$_SESSION["message"]=$message;
+  		   	header("location: request.php");
+      			exit;
+
+
+} ?>
+</div><!-- jumbotron-->
+</div><!-- container theme-showcase-->
+<?php
+function show_all_request(){
+      $json = file_get_contents('https://api.mlab.com/api/1/databases/crma51/collections/request?apiKey='.MLAB_API_KEY);
+      $data = json_decode($json);
+      $isData=sizeof($data);
+      if($isData >0){
+        $i=0;
+        ?>
+          <div class="panel panel-success">
+            <div class="panel-heading">
+              <h3 class="panel-title">รายการแจ้งคณะกรรมการรุ่น</h3>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-sm table-hover table-striped">
+                  <thead>
+                    <tr>
+                      <th>ลำดับ</th><th>เรื่อง</th>
+            <th>ผู้แจ้ง</th>
+          <th>ความเร่งด่วน</th>
+        <th>ประเภท</th>
+      <th>สถานะ</th></tr>
+          </thead><tbody>
+        <?php
+        foreach($data as $rec){
+          $i++;
+          $_id=$rec->_id;
+           $name=$rec->name;
+           $title=$rec->title;
+           $detail=$rec->detail;
+           $type=$rec->type;
+           $urgent=$rec->urgent;
+           $status=$rec->status;
+           ?>
+      <tr><td><?php echo $i;?></td>
+      <td class="text-nowrap"><?php echo $title;?></td>
+      <td class="text-nowrap"><?php echo $name;?></td>
+      <td><?php echo $urgent;?></td>
+      <td><?php echo $type;?></td>
+      <td><?php echo $status;?> <a href="request.php?action=review&id=<?php echo $_id;?>">รายละเอียด</a></td>
+      </tr>
+           <?php    } //end foreach ?>
+           </tbody>
+         </table>
+     </div><!-- class="table-responsive"> -->
+     </div><!-- class="panel panel-success"> -->
+           <?php
+           }else{
+           echo "ยังไม่มีข้อมูลแจ้งแก้ไขค่ะ";
+               }
+             }// end function show_friend
+               ?>
+
+               
+<?php function review_request($_id){
+  $json = file_get_contents('https://api.mlab.com/api/1/databases/crma51/collections/request/'.$_id.'?apiKey='.MLAB_API_KEY);
+  $data = json_decode($json);
+  $isData=sizeof($data);
+  if($isData >0){
+    $i=0;
+    foreach($data as $rec){
+      $i++;
+      $_id=$rec->_id;
+       $name=$rec->name;
+       $title=$rec->title;
+       $detail=$rec->detail;
+       $type=$rec->type;
+       $urgent=$rec->urgent;
+       $status=$rec->status;
+    ?>
+      <div class="panel panel-success">
+        <div class="panel-heading">
+          <h3 class="panel-title">รายการแจ้งคณะกรรมการรุ่น</h3>
+        </div>
+        <div class="table-responsive">
+            <table class="table table-sm table-hover table-striped">
+  <tr><td class="text-nowrap"><?php echo $title;?></td></tr>
+  <tr><td class="text-nowrap"><?php echo $name;?></td></tr>
+  <tr><td><?php echo $urgent;?></td></tr>
+  <tr><td><?php echo $type;?></td></tr>
+<tr><td><?php echo $detail;?></td></tr>
+  <tr><td><?php echo $status;?> <a href="request.php?action=review&id=<?php echo $_id;?>">รายละเอียด</a></td></tr>
+</table>
+</div>
+</div>
+       <?php    } //end foreach ?>
+
+}
+?>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+<!-- Latest compiled and minified Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</body>
+</html>
