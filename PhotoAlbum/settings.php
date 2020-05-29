5cd230e4b777f42899b22919fb99d4354a194f83
Onion <popwandee@gmail.com>
2020-05-29 19:28:24 +0700

    update

--- PhotoAlbum/settings.php
+++ PhotoAlbum/settings.php
@@ -1,8 +1,8 @@
 <?php
 \Cloudinary::config(array(
-    'cloud_name' => 'my_cloud_name',
-    'api_key' => '123456789012345',
-    'api_secret' => 'abcdefghijklmnopqrstuvwxyz1'
+  'cloud_name' => 'dly6ftryr',
+  'api_key' => '979642835457647',
+  'api_secret' => 'hOm8JS7iHanEnFaqB9Y7qDwT5CQ'
 ));
 
 R::setup('mysql:host=my_database.mydomain.com;dbname=photo_album', 'username', 'password');
