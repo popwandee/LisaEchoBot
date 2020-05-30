64398a24a571413919dbcc8c7e36bf909d4e787c
Onion <popwandee@gmail.com>
2020-05-30 22:27:21 +0700

    debug

--- PhotoAlbum/autoloader.php
+++ PhotoAlbum/autoloader.php
@@ -1,9 +1,9 @@
 <?php
 
-if (is_file(__DIR__ . '/../../vendor/autoload.php') && is_readable(__DIR__ . '/../../vendor/autoload.php')) {
-    require_once __DIR__.'/../../vendor/autoload.php';
+if (is_file(__DIR__ . 'vendor/cloudinary/cloudinary_php/autoload.php') && is_readable(__DIR__ . 'vendor/cloudinary/cloudinary_php/autoload.php')) {
+    require_once __DIR__.'vendor/cloudinary/cloudinary_php/autoload.php';
 } else {
     // Fallback to legacy autoloader
-    require_once __DIR__.'/../../autoload.php';
-    require_once __DIR__.'/../../src/Helpers.php';
+    require_once __DIR__.'vendor/cloudinary/cloudinary_php/autoload.php';
+    require_once __DIR__.'vendor/cloudinary/cloudinary_php/src/Helpers.php';
 }
