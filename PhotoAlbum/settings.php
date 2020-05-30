0e3a0f98fe49d14a119ffdc1f0964940c5f57fd9
Onion <popwandee@gmail.com>
2020-05-30 21:42:28 +0700

    update

--- PhotoAlbum/settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-\Cloudinary::config(array(
-  'cloud_name' => 'dly6ftryr',
-  'api_key' => '979642835457647',
-  'api_secret' => 'hOm8JS7iHanEnFaqB9Y7qDwT5CQ'
-));
-
-R::setup('mysql:host=my_database.mydomain.com;dbname=photo_album', 'username', 'password');
--- cloudinary.php
+++ cloudinary.php
@@ -41,5 +41,4 @@ if(isset($_POST['submit'])){
                 <input type='submit' value='Submit' class='btn btn-primary' /></td></tr>
            </table>
        </form>
-<img src="https://res.cloudinary.com/dly6ftryr/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1590735946/95736235_157455389101365_4497114901063401472_o_cquej7.jpg">
        <?php echo cl_image_tag('img_crma51');?>
