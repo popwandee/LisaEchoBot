f0bcc9d89d3980b01b0fe7ce7641ffcf233d9ee8
Onion <popwandee@gmail.com>
2020-05-30 23:21:53 +0700

    update

--- cloudinary.php
+++ cloudinary.php
@@ -8,8 +8,12 @@ if(isset($_POST['submit'])){
   \Cloudinary\Uploader::upload($file_tmp,array($name=>$slug));
 }
 ?>
+<div class="card">
+<label class="file-upload-container" for="file-upload">
 <input id="file-upload" type="file" style="display:none;">
 Select an Image
+</label>
+</div>
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
       <table class="table table-sm table-hover table-striped" width="300">
      <tr><td colspan="2">แนบรูปภาพ<input type='file' name='file' class='form-control' /></td></tr>
