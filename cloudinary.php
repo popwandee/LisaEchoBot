24cd22c2ae54013bd3b3e00040e5cd765e45b233
Onion <popwandee@gmail.com>
2020-05-31 00:01:46 +0700

    update

--- cloudinary.php
+++ cloudinary.php
@@ -32,7 +32,13 @@
 <body>
 <?php
 require __DIR__ .'/lib/rb.php';
-require 'autoloader.php';
+if (is_file(__DIR__ . 'vendor/cloudinary/cloudinary_php/autoload.php') && is_readable(__DIR__ . 'vendor/cloudinary/cloudinary_php/autoload.php')) {
+    require_once __DIR__.'vendor/cloudinary/cloudinary_php/autoload.php';
+} else {
+    // Fallback to legacy autoloader
+    require_once __DIR__.'vendor/cloudinary/cloudinary_php/autoload.php';
+    require_once __DIR__.'vendor/cloudinary/cloudinary_php/src/Helpers.php';
+}
 
 error_reporting(E_ALL | E_STRICT);
 
