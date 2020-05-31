c774ef18f647f030eaf97199dca916296e754d15
Onion <popwandee@gmail.com>
2020-05-31 16:48:42 +0700

    แก้ไข

--- friend_preview.php
+++ friend_preview.php
@@ -136,8 +136,8 @@ $user_id = isset($_SESSION["user_id"]) ? $_SESSION["user_id"] : "";
               }else{ // ไม่ได้มาจากหอร์ม แสดงฟอร์ม
                 echo "<a href='friend_preview.php?_id=$_id&action=preview'><button type='button' class='btn btn-xs btn-success'>แสดงตัวอย่าง</button></a>";
                 echo "user_id is ".$user_id." _id is ".$_id;
-                if($user_id==$_id){ echo "คุณสามารถแก้ไขข้อมูลตัวเองได้";}
-                if(($_SESSION['type'])=='admin'){echo "Admin แก้ไขข้อูลเพื่อน ๆ ได้";}
+                if($user_id==$_id){ echo "คุณสามารถแก้ไขข้อ มูลตัวเองได้";}
+                if(($_SESSION['type'])=='admin'){echo "Admin แก้ไขข้อมูลเพื่อน ๆ ได้";}
                 show_form($_id);
               }// end if isset formSubmit
 
