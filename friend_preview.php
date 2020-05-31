59a77064fb00883fae04696bd1bc558cd1baaf91
Onion <popwandee@gmail.com>
2020-05-31 15:13:46 +0700

    debug

--- friend_preview.php
+++ friend_preview.php
@@ -1,17 +1,6 @@
 <?php
 // Initialize the session
 session_start();
-/*
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
-}
-if(!isset($_SESSION["type"]) || $_SESSION["type"] == "สมาชิก"){
-    header("location: index.php?message=You are not admin.");
-    exit;
-}
-*/
 
 // Cloudinary
 require 'vendor/cloudinary/cloudinary_php/src/Cloudinary.php';
@@ -22,6 +11,17 @@ require 'vendor/cloudinary/cloudinary_php/src/Api.php';
 require_once "config.php";// mlab
 require_once "vendor/autoload.php";
 require_once "vendor/function.php";
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+if(!isset($_SESSION["type"]) || $_SESSION["type"] == "สมาชิก"){
+    header("location: index.php?message=You are not admin.");
+    exit;
+}
+
 
 $user_id = isset($_SESSION["user_id"]) ? $_SESSION["user_id"] : "";
 ?>
