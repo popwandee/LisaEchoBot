725cb4b6ac0c2f93d8aa47d80bc37a82e513b547
Onion <popwandee@gmail.com>
2020-05-31 12:00:52 +0700

    debug test

--- test.php
+++ test.php
@@ -5,6 +5,9 @@ require 'vendor/cloudinary/cloudinary_php/src/Api.php';
 require 'vendor/settings.php';
 
 
+?>
+<?php
+
 if (isset($_POST["submit"])) {
     print_r($_FILES["fileToUpload"]);
     $cloudUpload = \Cloudinary\Uploader::upload($_FILES["fileToUpload"]['tmp_name']);
@@ -15,29 +18,15 @@ if (isset($_POST["submit"])) {
 <!DOCTYPE HTML>
   <html>
     <head>
-      <!-- script for Cloudinaty -->
-      <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'></script>
-      <script src='jquery.ui.widget.js' type='text/javascript'></script>
-      <script src='jquery.iframe-transport.js' type='text/javascript'></script>
-      <script src='jquery.fileupload.js' type='text/javascript'></script>
-      <script src='jquery.cloudinary.js' type='text/javascript'></script>
-
   </head>
 
   <body>
-    <?php echo cloudinary_js_config(); ?>
-        <?php
-          if (array_key_exists('REQUEST_SCHEME', $_SERVER)) {
-            $cors_location = $_SERVER["REQUEST_SCHEME"] . "://" . $_SERVER["SERVER_NAME"] .
-          dirname($_SERVER["SCRIPT_NAME"]) . "/cloudinary_cors.html";
-          } else {
-            $cors_location = "http://" . $_SERVER["HTTP_HOST"] . "/cloudinary_cors.html";
-          }
-      ?>
 
-        <form action="uploaded.php" method="post">
-          <?php echo cl_image_upload_tag('image_id', array("callback" => $cors_location)); ?>
-        </form>
+<form method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
 
   </body>
 </html>
