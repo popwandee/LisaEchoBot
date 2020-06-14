50ff9edd11978464fa1a95ffb2341bdc5c9cd415
Onion <popwandee@gmail.com>
2020-06-14 21:37:38 +0700

    update

--- km.php
+++ km.php
@@ -166,7 +166,7 @@ switch ($action) {
     break;
   default:
     new_post_form();
-    $json = file_get_contents('https://api.mlab.com/api/1/databases/crma51/collections/finance?apiKey='.MLAB_API_KEY.'&s={"type":1}');
+    $json = file_get_contents('https://api.mlab.com/api/1/databases/crma51/collections/finance?apiKey='.MLAB_API_KEY.'&s={"date":-1}');
     $data = json_decode($json);
     $isData=sizeof($data);
              //$textReplyMessage= $textReplyMessage." isData ".$isData." ค่ะ\n\n";
