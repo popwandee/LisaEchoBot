0e1f8b59955e8432c2c4b2a58c8a393ec5723253
popwandee <popwandee@gmail.com>
2020-12-16 21:14:50 +0700

    Update testRestIO.php

--- testRestIO.php
+++ testRestIO.php
@@ -1,20 +1,13 @@
 
-<?php // core logic
-setUrl('https://area51-dfba.restdb.io/rest/people');
-$request->setMethod(HTTP_METH_GET);
-
-$request->setHeaders(array(
-'cache-control' => 'no-cache',
-'x-apikey' => '5fd9fb83ff9d670638140649',
-'content-type' => 'application/json'
-));
-
-try {
-    $response = $request->send();
-    echo $response->getBody();
-} catch (HttpException $ex) {
-    echo $ex;
-    print_r($ex);
-}
-           
+<?php 
+$url="https://area51-dfba.restdb.io/rest/people";
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','x-apikey: d376fad73437100d1a070319e29d9b12fbe21')')
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            $return_data = curl_exec($ch);
+            curl_close($ch);
+            return json_decode($return_data,TRUE);
+            
+            print_r($return_data);
+            
 ?>
