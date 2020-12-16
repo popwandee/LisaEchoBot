4f92bf36d86ab29235417761b23036996d77eb41
popwandee <popwandee@gmail.com>
2020-12-16 21:03:49 +0700

    Update testRestIO.php

--- testRestIO.php
+++ testRestIO.php
@@ -1,24 +1,6 @@
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="Relax Website">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-
-    <title>AFAPS40-CRMA51</title>
-  
-</head>
-<body>
-   
-
 <?php // core logic
-
-  setUrl('https://area51-dfba.restdb.io/rest/people?q={"name": "ไพศาล"}');
+setUrl('https://area51-dfba.restdb.io/rest/people');
 $request->setMethod(HTTP_METH_GET);
 
 $request->setHeaders(array(
@@ -35,5 +17,3 @@ try {
 }
            
 ?>
-</body>
-</html>
