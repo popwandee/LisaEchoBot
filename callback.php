9d345e4df84a36eb760418664aa195a681794fcd
Onion <popwandee@gmail.com>
2021-01-14 22:59:47 +0700

    test

--- callback.php
+++ callback.php
@@ -150,8 +150,8 @@ foreach ($events as $event) {
                     // กำหนด action 4 ปุ่ม 4 ประเภท
                     $actionBuilder = array(
                         new MessageTemplateActionBuilder(
-                            $province,//'Message Template',// ข้อความแสดงในปุ่ม
-                            'This is Text' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
+                            'เพื่อนๆในจังหวัด '.$province,//'Message Template',// ข้อความแสดงในปุ่ม
+                            '$'.$province // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
                         ),
                         new UriTemplateActionBuilder(
                             'Uri Template', // ข้อความแสดงในปุ่ม
