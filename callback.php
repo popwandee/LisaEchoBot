d687c5005a035d95f7989b4ba59df6f8abfa61bb
Onion <popwandee@gmail.com>
2021-01-14 14:14:48 +0700

    test

--- callback.php
+++ callback.php
@@ -195,6 +195,196 @@ foreach ($events as $event) {
           $multiMessage->add($textMessage);
           $replyData = $multiMessage;
       }// end elseif $
+      elseif($text[0]=='='){
+        $textReplyMessage ='{
+          "type": "carousel",
+          "contents": [
+            {
+              "type": "bubble",
+              "hero": {
+                "type": "image",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
+                "size": "full",
+                "aspectRatio": "20:13",
+                "aspectMode": "cover"
+              },
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Arm Chair, White",
+                    "weight": "bold",
+                    "size": "xl",
+                    "wrap": true,
+                    "contents": []
+                  },
+                  {
+                    "type": "box",
+                    "layout": "baseline",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "$49",
+                        "weight": "bold",
+                        "size": "xl",
+                        "flex": 0,
+                        "wrap": true,
+                        "contents": []
+                      },
+                      {
+                        "type": "text",
+                        "text": ".99",
+                        "weight": "bold",
+                        "size": "sm",
+                        "flex": 0,
+                        "wrap": true,
+                        "contents": []
+                      }
+                    ]
+                  }
+                ]
+              },
+              "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "uri",
+                      "label": "Add to Cart",
+                      "uri": "https://linecorp.com"
+                    },
+                    "style": "primary"
+                  },
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "uri",
+                      "label": "Add to wishlist",
+                      "uri": "https://linecorp.com"
+                    }
+                  }
+                ]
+              }
+            },
+            {
+              "type": "bubble",
+              "hero": {
+                "type": "image",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png",
+                "size": "full",
+                "aspectRatio": "20:13",
+                "aspectMode": "cover"
+              },
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Metal Desk Lamp",
+                    "weight": "bold",
+                    "size": "xl",
+                    "wrap": true,
+                    "contents": []
+                  },
+                  {
+                    "type": "box",
+                    "layout": "baseline",
+                    "flex": 1,
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "$11",
+                        "weight": "bold",
+                        "size": "xl",
+                        "flex": 0,
+                        "wrap": true,
+                        "contents": []
+                      },
+                      {
+                        "type": "text",
+                        "text": ".99",
+                        "weight": "bold",
+                        "size": "sm",
+                        "flex": 0,
+                        "wrap": true,
+                        "contents": []
+                      }
+                    ]
+                  },
+                  {
+                    "type": "text",
+                    "text": "Temporarily out of stock",
+                    "size": "xxs",
+                    "color": "#FF5551",
+                    "flex": 0,
+                    "margin": "md",
+                    "wrap": true,
+                    "contents": []
+                  }
+                ]
+              },
+              "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "uri",
+                      "label": "Add to Cart",
+                      "uri": "https://linecorp.com"
+                    },
+                    "flex": 2,
+                    "color": "#AAAAAA",
+                    "style": "primary"
+                  },
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "uri",
+                      "label": "Add to wish list",
+                      "uri": "https://linecorp.com"
+                    }
+                  }
+                ]
+              }
+            },
+            {
+              "type": "bubble",
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "uri",
+                      "label": "See more",
+                      "uri": "https://linecorp.com"
+                    },
+                    "flex": 1,
+                    "gravity": "center"
+                  }
+                ]
+              }
+            }
+          ]
+      }';
+
+        $textMessage = new TextMessageBuilder($textReplyMessage);
+        $multiMessage->add($textMessage);
+        $replyData = $multiMessage;
+    }// end elseif =
         elseif($text[0]=='!'){
           $sentence=substr($text,1); // ตัด $ ตัวแรกออก
           $words=explode(",",$sentence);
@@ -232,20 +422,10 @@ foreach ($events as $event) {
                      // $img_url = isset($rec['answer'])?$rec['answer']:"";
                   }//end for each
               }
-              /*
-              if(isset($img_url)){
-                  $image = new ImageMessageBuilder($img_url);
-                  $multiMessage->add($image);
-              }
-*/
+
               $textMessage = new TextMessageBuilder($textReplyMessage);
               $multiMessage->add($textMessage);
-
-              $flexMessage=setFlexTemplate();
-              //$textMessage = new FlexMessageBuilder($flexMessage);
-
-              $replyData = $flexMessage;
-              //$replyData = $multiMessage;
+              $replyData = $multiMessage;
             }
         }//end if else
 
@@ -265,190 +445,7 @@ function welcome($H){
    }
 }
 function setFlexTemplate(){
-$flexMessage='{
-  "type": "carousel",
-  "contents": [
-    {
-      "type": "bubble",
-      "hero": {
-        "type": "image",
-        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
-        "size": "full",
-        "aspectRatio": "20:13",
-        "aspectMode": "cover"
-      },
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "spacing": "sm",
-        "contents": [
-          {
-            "type": "text",
-            "text": "Arm Chair, White",
-            "weight": "bold",
-            "size": "xl",
-            "wrap": true,
-            "contents": []
-          },
-          {
-            "type": "box",
-            "layout": "baseline",
-            "contents": [
-              {
-                "type": "text",
-                "text": "$49",
-                "weight": "bold",
-                "size": "xl",
-                "flex": 0,
-                "wrap": true,
-                "contents": []
-              },
-              {
-                "type": "text",
-                "text": ".99",
-                "weight": "bold",
-                "size": "sm",
-                "flex": 0,
-                "wrap": true,
-                "contents": []
-              }
-            ]
-          }
-        ]
-      },
-      "footer": {
-        "type": "box",
-        "layout": "vertical",
-        "spacing": "sm",
-        "contents": [
-          {
-            "type": "button",
-            "action": {
-              "type": "uri",
-              "label": "Add to Cart",
-              "uri": "https://linecorp.com"
-            },
-            "style": "primary"
-          },
-          {
-            "type": "button",
-            "action": {
-              "type": "uri",
-              "label": "Add to wishlist",
-              "uri": "https://linecorp.com"
-            }
-          }
-        ]
-      }
-    },
-    {
-      "type": "bubble",
-      "hero": {
-        "type": "image",
-        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png",
-        "size": "full",
-        "aspectRatio": "20:13",
-        "aspectMode": "cover"
-      },
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "spacing": "sm",
-        "contents": [
-          {
-            "type": "text",
-            "text": "Metal Desk Lamp",
-            "weight": "bold",
-            "size": "xl",
-            "wrap": true,
-            "contents": []
-          },
-          {
-            "type": "box",
-            "layout": "baseline",
-            "flex": 1,
-            "contents": [
-              {
-                "type": "text",
-                "text": "$11",
-                "weight": "bold",
-                "size": "xl",
-                "flex": 0,
-                "wrap": true,
-                "contents": []
-              },
-              {
-                "type": "text",
-                "text": ".99",
-                "weight": "bold",
-                "size": "sm",
-                "flex": 0,
-                "wrap": true,
-                "contents": []
-              }
-            ]
-          },
-          {
-            "type": "text",
-            "text": "Temporarily out of stock",
-            "size": "xxs",
-            "color": "#FF5551",
-            "flex": 0,
-            "margin": "md",
-            "wrap": true,
-            "contents": []
-          }
-        ]
-      },
-      "footer": {
-        "type": "box",
-        "layout": "vertical",
-        "spacing": "sm",
-        "contents": [
-          {
-            "type": "button",
-            "action": {
-              "type": "uri",
-              "label": "Add to Cart",
-              "uri": "https://linecorp.com"
-            },
-            "flex": 2,
-            "color": "#AAAAAA",
-            "style": "primary"
-          },
-          {
-            "type": "button",
-            "action": {
-              "type": "uri",
-              "label": "Add to wish list",
-              "uri": "https://linecorp.com"
-            }
-          }
-        ]
-      }
-    },
-    {
-      "type": "bubble",
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "spacing": "sm",
-        "contents": [
-          {
-            "type": "button",
-            "action": {
-              "type": "uri",
-              "label": "See more",
-              "uri": "https://linecorp.com"
-            },
-            "flex": 1,
-            "gravity": "center"
-          }
-        ]
-      }
-    }
-  ]
-}' ;
+$flexMessage='' ;
 return $flexMessage;
 }
 function getRandomGallery($json){
