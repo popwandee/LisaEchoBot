5a98bf2fd8021460dff6f7917524e4fa5e62f4fc
Onion <popwandee@gmail.com>
2021-01-17 15:29:28 +0700

    test

--- callback.php
+++ callback.php
@@ -337,7 +337,7 @@ foreach ($events as $event) {
                                  $actionBuilder
                              );
                              $count ++;
-                             if($count<7){
+                             if($count<8){
                                 array_push($arr,$newCarousel);
                              }
 
